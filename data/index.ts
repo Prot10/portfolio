1a3b80964a88665fed3ef6a8860110cc52466203
Andrea Protani <andrea.protani@outlook.it>
2026-01-19 23:01:54 +0100

    content: update grid to showcase MyMacCleaner project

--- data/index.ts
+++ data/index.ts
@@ -51,8 +51,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Trying to improve my front-end skills",
-    description: "Building websites as a side job",
+    title: "Currently building MyMacCleaner",
+    description: "Open-source macOS utility for system optimization",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
