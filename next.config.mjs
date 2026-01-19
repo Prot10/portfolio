55b8349fc96ed617d72745b4ecc2119b298acf14
Andrea Protani <andrea.protani@outlook.it>
2026-01-19 22:46:51 +0100

    chore: remove Sentry error tracking

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { withSentryConfig } from '@sentry/nextjs';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -8,14 +6,4 @@ const nextConfig = {
   output: 'export',
 };
 
-export default withSentryConfig(nextConfig, {
-  silent: true,
-  org: "javascript-mastery",
-  project: "javascript-nextjs",
-}, {
-  widenClientFileUpload: true,
-  transpileClientSDK: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+export default nextConfig;
